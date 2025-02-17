b34b4e198f5efa9a4a8480bebea2962cabb05022
Júlio César Biz <juliocesarbiz@hotmail.com>
2025-02-17 12:54:43 -0300

    feat: UserRepo

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -3,7 +3,7 @@ from src.infra.db.entities.users import Users as UsersEntity
 class UserRepository:
 
     @classmethod
-    def insert(cls, first_name: str, last_name: str, age: int) -> None:
+    def insert_user(cls, first_name: str, last_name: str, age: int) -> None:
         with DBConnectionHandler() as database:
             try:
                 new_registry = UsersEntity(
@@ -17,3 +17,17 @@ class UserRepository:
                 database.session.rollback()
                 raise exception
 
+    @classmethod
+    def select_user(cls, first_name: str) -> any:
+        with DBConnectionHandler() as database:
+            try:
+                users = (
+                    database.session
+                        .query(UsersEntity)
+                        .filter(UsersEntity.first_name == first_name)
+                        .all()
+                )
+                return users
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
--- src/infra/db/tests/users_repository_test.py
+++ src/infra/db/tests/users_repository_test.py
@@ -1,11 +1,55 @@
-#from src.infra.db.settings.connection import DBConnectionHandler
+import pytest
+from sqlalchemy import text
+from src.infra.db.settings.connection import DBConnectionHandler
 from src.infra.db.repositories.users_repository import UserRepository
 
+
+db_connection_handler = DBConnectionHandler()
+connection = db_connection_handler.get_engine().connect()
+
+@pytest.mark.skip(reason='Sensive test')
 def test_insert_user():
     mocked_first_name = 'first'
     mocked_last_name = 'last'
     mocked_age = 30
 
     user_repo = UserRepository()
-    user_repo.insert(mocked_first_name, mocked_last_name, mocked_age)
-    
+    user_repo.insert_user(mocked_first_name, mocked_last_name, mocked_age)
+
+
+    sql = '''
+        select * from clean_database.users
+        where first_name = '{}'
+        AND last_name = '{}'
+        AND age = {}
+    '''.format(mocked_first_name, mocked_last_name, mocked_age)
+
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(text(f'''
+    DELETE FROM clean_database.users where id = {registry.id}
+    '''))
+
+    connection.commit()
+
+def test_select_user():
+    mocked_first_name = 'first2'
+    mocked_last_name = 'last2'
+    mocked_age = 100
+
+    sql =  '''
+        INSERT INTO clean_database.users (FIRST_NAME, LAST_NAME, AGE) VALUES ('{}', '{}', {})
+    '''.format(mocked_first_name, mocked_last_name, mocked_age)
+
+    connection.execute(sql)
+    #connection.commit()
+
+    user_repository = UserRepository()
+    response = user_repository.select_user(mocked_first_name)
+    print("Test Select")
+    print(response)
