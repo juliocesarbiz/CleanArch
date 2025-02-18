ec359e9fc74dcfec3a369b1053f3350761bb6e86
Júlio César Biz <juliocesarbiz@hotmail.com>
2025-02-17 22:16:26 -0300

    feat: Implementing use_case final

--- src/data/tests/user_finder_test.py
+++ src/data/tests/user_finder_test.py
@@ -12,4 +12,45 @@ def test_find():
 
     assert response['type'] == 'Users'
     assert response['count'] == len(response['attributes'])
-    assert response['attributes'] != []
+    assert response['attributes']
+
+def test_find_error_in_valid_name():
+    first_name = 'meuNome123'
+
+    repo = UserRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Nome invalido para a busca"
+
+
+def test_find_error_in_long_name():
+    first_name = 'meuNomeewefewfewfwefewfewfewfewfewfewfwefewfewfw'
+
+    repo = UserRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Nome muito grande para a busca"
+
+
+def test_find_error_user_not_found():
+    class UsersRepositoryError(UserRepositorySpy):
+        def select_user(self, first_name: str):
+            return []
+        
+    first_name = 'meuNome'
+    repo = UsersRepositoryError()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Usuario nao encontrado"
