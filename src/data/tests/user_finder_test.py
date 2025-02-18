5a244d58a6b9cd0b49fa3527cb46f27d6462b687
Júlio César Biz <juliocesarbiz@hotmail.com>
2025-02-17 21:29:29 -0300

    feat: Starting implementing use_case

--- src/data/tests/user_finder_test.py
+++ src/data/tests/user_finder_test.py
@@ -1,6 +1,15 @@
+from src.infra.db.tests.user_repository import UserRepositorySpy
 from src.data.use_cases.user_finder import UserFinder
-from src.infra.db.repositories.users_repository import UserRepository
-
 def test_find():
-    repo = UserRepository()
+    first_name = 'meuNome'
+
+    repo = UserRepositorySpy()
     user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+
+    assert repo.select_user_attributes['first_name'] == first_name
+
+    assert response['type'] == 'Users'
+    assert response['count'] == len(response['attributes'])
+    assert response['attributes'] != []
--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -1,3 +1,5 @@
+#pylint: disable=broad-exception-raised
+
 from typing import Dict
 from src.domain.use_cases.user_finder import UserFinder as UserFinderInterface
 from src.data.interfaces.users_repository import UserRepositoryInterface
@@ -7,5 +9,23 @@ class UserFinder(UserFinderInterface):
         self.__users_repository = users_repository
 
     def find(self, first_name: str) -> Dict:
-        pass
+        if not first_name.isalpha():
+            raise Exception('Nome invalido para a busca')
+        
+        if len(first_name) > 18:
+            raise Exception('Nome muito grande para a busca')
+        
+        users = self.__users_repository.select_user(first_name)
+
+        if users == []:
+            raise Exception('Usuario nao encontrado')
+        
+        response = {
+                'type':'Users',
+                'count':len(users),
+                'attributes':users,
+        }
+
+        return response
  
+
